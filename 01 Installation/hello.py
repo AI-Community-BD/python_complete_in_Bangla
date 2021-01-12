1a6bc14f5c1d3f239ec387e1254477ea5e29091f
Ananta Asim <anantajoy007@gmail.com>
2021-01-12 11:29:50 +0600

    python installation and environment set up

---
+++ 01 Installation/hello.py
@@ -0,0 +1,10 @@
+# Print messages in your screen
+print("Hello World!")
+print("My first code in Python")
+print("My first code in VS Code")
+
+# sum of number
+print(6 + 4) # Answer 10
+print(6 - 4) # Answer 2
+print(6 * 4) # Answer 24
+
